b0f2e900ff741a1e9acdd73ac4eda57f9314cdeb
voyadger01 <voyadgerkx@gmail.com>
2026-06-25 11:18:37 +0300

    add reader

--- karpovich.dmitriy/U1/io.cpp
+++ karpovich.dmitriy/U1/io.cpp
@@ -13,6 +13,48 @@ bool karpovich::containsPerson(const Vector< Person > &persons, size_t id)
   return false;
 }
 
+bool karpovich::readPersons(std::istream &input, Vector< Person > &persons, size_t &validCount, size_t &ignoredCount)
+{
+  validCount = 0;
+  ignoredCount = 0;
+  std::string line;
+  while (std::getline(input, line)) {
+    size_t pos = 0;
+    while (pos < line.size() && std::isspace(line[pos])) {
+      ++pos;
+    }
+    size_t id = 0;
+    size_t idBegin = pos;
+    while (pos < line.size() && std::isdigit(line[pos])) {
+      id = id * 10 + static_cast< size_t >(line[pos] - '0');
+      ++pos;
+    }
+    if (idBegin == pos) {
+      ++ignoredCount;
+      continue;
+    }
+    while (pos < line.size() && std::isspace(line[pos])) {
+      ++pos;
+    }
+    if (pos == line.size()) {
+      ++ignoredCount;
+      continue;
+    }
+    if (containsPerson(persons, id)) {
+      ++ignoredCount;
+      continue;
+    }
+    Person person;
+    person.id = id;
+    person.info = line.substr(pos);
+    if (!pushBack(persons, person)) {
+      return false;
+    }
+    ++validCount;
+  }
+  return true;
+}
+
 void karpovich::writePersons(std::ostream &output, const Vector< Person > &persons)
 {
   for (size_t i = 0; i < persons.size; ++i) {
