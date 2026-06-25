3d6d177bfe984df1992099a37bfb4f2a02c18dd9
voyadger01 <voyadgerkx@gmail.com>
2026-06-25 12:50:07 +0300

    add other funcs

--- karpovich.dmitriy/U2/meet.cpp
+++ karpovich.dmitriy/U2/meet.cpp
@@ -216,3 +216,130 @@ void karpovich::desc(std::ostream &output, const Vector< Person > &persons, size
     output << person->info << '\n';
   }
 }
+
+void karpovich::commons(std::ostream &output, const Vector< Meet > &meets, size_t id1, size_t id2)
+{
+  Vector< size_t > contacts1;
+  Vector< size_t > contacts2;
+  Vector< size_t > result;
+
+  initVector(contacts1);
+  initVector(contacts2);
+  initVector(result);
+
+  for (size_t i = 0; i < meets.size; ++i) {
+    if (meets.data[i].firstId == id1) {
+      pushBack(contacts1, meets.data[i].secondId);
+    } else if (meets.data[i].secondId == id1) {
+      pushBack(contacts1, meets.data[i].firstId);
+    }
+    if (meets.data[i].firstId == id2) {
+      pushBack(contacts2, meets.data[i].secondId);
+    } else if (meets.data[i].secondId == id2) {
+      pushBack(contacts2, meets.data[i].firstId);
+    }
+  }
+  for (size_t i = 0; i < contacts1.size; ++i) {
+    bool found = false;
+    for (size_t j = 0; j < contacts2.size; ++j) {
+      if (contacts1.data[i] == contacts2.data[j]) {
+        found = true;
+        break;
+      }
+    }
+    if (found) {
+      bool duplicate = false;
+      for (size_t j = 0; j < result.size; ++j) {
+        if (result.data[j] == contacts1.data[i]) {
+          duplicate = true;
+          break;
+        }
+      }
+      if (!duplicate) {
+        pushBack(result, contacts1.data[i]);
+      }
+    }
+  }
+  for (size_t i = 0; i < result.size; ++i) {
+    for (size_t j = i + 1; j < result.size; ++j) {
+      if (result.data[j] < result.data[i]) {
+        size_t temp = result.data[i];
+        result.data[i] = result.data[j];
+        result.data[j] = temp;
+      }
+    }
+  }
+  for (size_t i = 0; i < result.size; ++i) {
+    output << result.data[i] << '\n';
+  }
+  destroyVector(contacts1);
+  destroyVector(contacts2);
+  destroyVector(result);
+}
+
+void karpovich::less(std::ostream &output, const Vector< Meet > &meets, size_t time, size_t id)
+{
+  Vector< MeetKey > result;
+  initVector(result);
+
+  for (size_t i = 0; i < meets.size; ++i) {
+    if (meets.data[i].firstId == id && meets.data[i].duration < time) {
+      MeetKey key;
+      key.id = meets.data[i].secondId;
+      key.duration = meets.data[i].duration;
+      pushBack(result, key);
+    } else if (meets.data[i].secondId == id && meets.data[i].duration < time) {
+      MeetKey key;
+      key.id = meets.data[i].firstId;
+      key.duration = meets.data[i].duration;
+      pushBack(result, key);
+    }
+  }
+
+  sortMeets(result);
+  for (size_t i = 0; i < result.size; ++i) {
+    output << result.data[i].id << ' ' << result.data[i].duration << '\n';
+  }
+  destroyVector(result);
+}
+
+void karpovich::greater(std::ostream &output, const Vector< Meet > &meets, size_t time, size_t id)
+{
+  Vector< MeetKey > result;
+  initVector(result);
+
+  for (size_t i = 0; i < meets.size; ++i) {
+    if (meets.data[i].firstId == id && meets.data[i].duration > time) {
+      MeetKey key;
+      key.id = meets.data[i].secondId;
+      key.duration = meets.data[i].duration;
+      pushBack(result, key);
+    } else if (meets.data[i].secondId == id && meets.data[i].duration > time) {
+      MeetKey key;
+      key.id = meets.data[i].firstId;
+      key.duration = meets.data[i].duration;
+      pushBack(result, key);
+    }
+  }
+  sortMeets(result);
+  for (size_t i = 0; i < result.size; ++i) {
+    output << result.data[i].id << ' ' << result.data[i].duration << '\n';
+  }
+  destroyVector(result);
+}
+
+void karpovich::outPersons(std::ostream &output, const Vector< Person > &persons)
+{
+  Vector< Person > withDescription;
+  initVector(withDescription);
+
+  for (size_t i = 0; i < persons.size; ++i) {
+    if (!persons.data[i].info.empty()) {
+      pushBack(withDescription, persons.data[i]);
+    }
+  }
+
+  writePersons(output, withDescription);
+
+  destroyVector(withDescription);
+}
