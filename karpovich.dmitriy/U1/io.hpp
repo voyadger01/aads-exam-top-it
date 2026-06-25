b672bca7e1934cb119540dbde7e8df0c71d77c89
voyadger01 <voyadgerkx@gmail.com>
2026-06-25 10:59:32 +0300

    add io

---
+++ karpovich.dmitriy/U1/io.hpp
@@ -0,0 +1,15 @@
+#ifndef KARPOVICH_IO_HPP
+#define KARPOVICH_IO_HPP
+
+#include <cstddef>
+#include <iosfwd>
+#include "person_array.hpp"
+
+namespace karpovich
+{
+  bool readPersons(std::istream &input, PersonArray &persons, size_t &validCount, size_t &ignoredCount);
+  void writePersons(std::ostream &output, const PersonArray &persons);
+  void writeStatistics(std::ostream &output, size_t validCount, size_t ignoredCount);
+}
+
+#endif
